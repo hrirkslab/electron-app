8a1f5e50319362446f66e778c5af3ef8fabdcff6
Ujwal Subedi <hrirkslab@gmail.com>
2024-08-20 03:44:19 +0000

    Updates

---
+++ static/js/198.824ffd20.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshift_todo=self.webpackChunkshift_todo||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.824ffd20.chunk.js.map
